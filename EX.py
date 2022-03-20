70ebb650a8eb27ca9a823740ed4fbbd8932426f6
totsukaTH <75716184+totsukaTH@users.noreply.github.com>
2022-03-20 15:04:55 +0700

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,23 @@
+a = 0
+#eval()
+#exec('klayut')
+#data = "'kla'"
+#data2 = 'data' +'='+'['+data+']'
+#print(data2)
+#a = exec(data2)
+#print(a)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": ['kla',0]
+}
+thisdict["year"] = ['kla',1,("162.654.255",1255)]
+print(thisdict["year"])
+
+
+
+
+
+
+input()
